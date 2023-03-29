9d6acf60388b181f762e1df7dc99bbbcff39d045
mhy <11912919@mail.sustech.edu.cn>
2023-03-30 01:15:14 +0800

    Add Swagger2 and CROS config

--- src/main/java/com/sustech/controller/UserController.java
+++ src/main/java/com/sustech/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.sustech.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ${author}
  * @since 2023-03-29
  */
+@Api(tags = "UserController")
 @RestController
 @RequestMapping("//user")
 public class UserController {
+
+    @ApiOperation(value = "demo")
+    @GetMapping("/demo")
+    public String demo() {
+        return "qx";
+    }
 
 }
 
