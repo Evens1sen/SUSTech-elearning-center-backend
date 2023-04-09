b1d49e9dac1c0379918aa36755f9864e2d7af626
mhy <11912919@mail.sustech.edu.cn>
2023-04-09 21:58:26 +0800

    Student calendar

---
+++ src/main/java/com/sustech/VO/CourseEventVO.java
@@ -0,0 +1,21 @@
+package com.sustech.VO;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class CourseEventVO {
+
+    private String courseCode;
+
+    private String courseName;
+
+    private String eventType;
+
+    private String eventName;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+}
--- src/main/java/com/sustech/controller/CourseEventController.java
+++ src/main/java/com/sustech/controller/CourseEventController.java
@@ -2,17 +2,24 @@ package com.sustech.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sustech.VO.CourseEventVO;
+import com.sustech.entity.Course;
 import com.sustech.entity.CourseEvent;
+import com.sustech.entity.User;
 import com.sustech.service.CourseEventService;
+import com.sustech.service.CourseService;
+import com.sustech.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,11 +38,41 @@ public class CourseEventController {
     @Autowired
     public CourseEventService courseEventService;
 
-    @ApiOperation(value = "获取课程所有event")
+    @Autowired
+    public CourseService courseService;
+
+    @Autowired
+    public UserService userService;
+
+    @ApiOperation(value = "获取学生的所有事件")
+    @GetMapping("/getAllCourseEvents/{userID}")
+    public List<CourseEventVO> getAllCourseEvents(@PathVariable Integer userID) {
+        User user = userService.getById(userID);
+        List<String> courseIDList = List.of(user.getCourseList().split(","));
+        List<Course> courseList = courseService.listByIds(courseIDList);
+
+        List<CourseEventVO> courseEventVOList = new ArrayList<>();
+        for (Course course : courseList) {
+            List<CourseEvent> courseEventList = listCourseEvent(course.getCourseId());
+            for (CourseEvent courseEvent : courseEventList) {
+                CourseEventVO courseEventVO = new CourseEventVO();
+                courseEventVO.setCourseCode(course.getCourseCode());
+                courseEventVO.setCourseName(course.getCourseName());
+                BeanUtils.copyProperties(courseEvent, courseEventVO);
+                courseEventVOList.add(courseEventVO);
+            }
+        }
+
+        return courseEventVOList;
+    }
+
+    @ApiOperation(value = "获取课程所有除了Assignment与Project的event")
     @GetMapping("/listCourseEvent/{courseID}")
     public List<CourseEvent> listCourseEvent(@PathVariable Integer courseID) {
         QueryWrapper<CourseEvent> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("course_id", courseID);
+        queryWrapper.ne("event_type", "Assignment");
+        queryWrapper.ne("event_type", "Project");
         return courseEventService.list(queryWrapper);
     }
 
