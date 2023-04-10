5f74d6355fbf662e0eb4be0a4a58ce217f3a5e17
杨一轩 <99703745+YeungYathin@users.noreply.github.com>
2023-04-10 22:23:45 +0800

    Query all threads posted by the user provided the userID.

--- src/main/java/com/sustech/controller/ThreadController.java
+++ src/main/java/com/sustech/controller/ThreadController.java
@@ -1,8 +1,17 @@
 package com.sustech.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sustech.entity.Thread;
+import com.sustech.service.ThreadService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +25,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("//thread")
 public class ThreadController {
 
+    @Autowired
+    public ThreadService threadService;
+
+    @ApiOperation(value = "获取某用户发布的所有Thread")
+    @GetMapping("/")
+    public List<Thread> listThreadsByUserID(@PathVariable Integer UserID){
+        QueryWrapper<Thread> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id", UserID);
+        return threadService.list(queryWrapper);
+    }
 }
 
