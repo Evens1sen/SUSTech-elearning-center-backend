6fc5a05bce81be6f1f749ed2d6d1424f5f2d9107
杨一轩 <99703745+YeungYathin@users.noreply.github.com>
2023-04-10 21:42:55 +0800

    Query all post by thread_id in PostController.java.

--- src/main/java/com/sustech/controller/PostController.java
+++ src/main/java/com/sustech/controller/PostController.java
@@ -1,8 +1,17 @@
 package com.sustech.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sustech.entity.Post;
+import com.sustech.service.PostService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +25,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("//post")
 public class PostController {
 
-}
+    @Autowired
+    public PostService postService;
 
+    @ApiOperation(value = "获取某thread所有post")
+    @GetMapping("/listAllPost/{threadID}")
+    public List<Post> listPostByThread(@PathVariable Integer threadID){
+        QueryWrapper<Post> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("thread_id", threadID);
+        return postService.list(queryWrapper);
+    }
+
+}
